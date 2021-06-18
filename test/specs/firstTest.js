c9e584625c27c76c90eff118b28b17dd3e5d44bd
imshaiknasir <skfiroj1994@gmail.com>
2021-06-19 03:18:01 +0530

    edited test file

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -1,6 +1,6 @@
 describe("testsuite 1",()=>{
     it ('test 1',()=>{
-        browser.url("https://google.com");
+        browser.url("https://www.cleartrip.com/");
         console.log(browser.getTitle());
     })
 })
